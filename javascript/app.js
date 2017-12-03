d898d8bcf3434a698dc263e427005b6bc8c7821b
josephskrzysowski <josephskrzysowski@gmail.com>
2017-12-03 13:05:58 -0500

    all done, kinda....GE

--- javascript/app.js
+++ javascript/app.js
@@ -10,26 +10,25 @@ $(document).ready(function() {
       async: false,
       dataType: "jsonp",
     }).done((res, err) => {
-      // console.log(res);
+      console.log(res);
       // console.log("res[1][0a]",res[1][0]);
       // console.log("res[2][0]",res[2][0]);
       // console.log("res[3][0]",res[3][0]);
+      $("#response").html("");
       for(var i = 0; i < res[1].length; i++) {
-	      $("#output").prepend(res[1][0]);
+	$("#response").append("<div id='newDiv'class='well'>" +
+			      "<h1><a href=' " + res[3][i]+ "'>"+
+			      res[1][i] +
+			      "</a></h1>"+
+			      "<h2>" + res[2][i] +
+			      "</h2>" +
+			      "</div>");
       }
-      $("#response").html("<div id='newDiv'>" +
-			  "<h1><a href=' " + res[3][0]+ "'>"+
-			  res[1][0] +
-			  "</a></h1>"+
-			  "<h2>" + res[2][0] + "</h2>" +  "</div>");
-			  
-      //$("#response").html("<div id='newDiv'>" + "<h1><a href=' " + res[3][0]+ "'</a>" + res[1][0] + "</h1>" + "<h2>" + res[2][0] + "</h2>" +  "</div>");
-      
       data.res = JSON.stringify(res);
     }).fail((jqXHR, textStatus, errorThrown) => {
-	     console.log(jqXHR.status);
-	     console.log(textStatus);
-	     console.log(errorThrown);
+      console.log(jqXHR.status);
+      console.log(textStatus);
+      console.log(errorThrown);
     }).always(() => {
       console.log("Complete");
     });
